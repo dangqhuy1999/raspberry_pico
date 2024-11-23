5c23726b2509c83a38de3f9fa1d6188de7236a0d
huydang <dangqhuy1999@gmail.com>
2024-11-23 22:03:55 +0700

    Antoandien1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# raspberry_pico
+# raspberry_pi_pico
+
+Mua bút thử điện
+Dây nóng, dây nguội, dây tiếp địa
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import machine
+import utime
 
+led_onboard = machine.Pin(25, machine.Pin.OUT)
+#huydang
+while True:
+  led_onboard.value(1)
+  utime.sleep_ms(1000)
+  led_onboard.value(0)
+  utime.sleep_ms(1000)
