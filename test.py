36e65b2dd13f1e9cd84c9a2814dfc87451081945
dangqhuy1999 <90665005+dangqhuy1999@users.noreply.github.com>
2024-11-29 11:57:17 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+import machine
+import utime  # Sử dụng utime thay vì time
+
+# Khởi tạo UART
+uart = machine.UART(0, baudrate=9600, tx=17, rx=16)  # Thay đổi chân theo cấu hình của bạn
+
+# Gửi lệnh phát bài nhạc 001_music.mp3 trong thư mục 01
+command = bytes([0x7E, 0xFF, 0x06, 0x03, 0x01, 0x01, 0x00, 0x7E])
+uart.write(command)
+
+# Đợi một chút để DFPlayer xử lý
+utime.sleep(5)  # Chờ 5 giây
