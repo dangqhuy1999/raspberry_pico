6123c8ba886088cd28902a627766e72da51c1b34
dangqhuy1999 <90665005+dangqhuy1999@users.noreply.github.com>
2024-11-29 13:26:25 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import machine
 import utime  # Sử dụng utime thay vì time
 
 # Khởi tạo UART
-uart = machine.UART(0, baudrate=9600, tx=17, rx=16)  # Thay đổi chân theo cấu hình của bạn
+uart = machine.UART(0, baudrate=9600, tx=16, rx=17)  # Thay đổi chân theo cấu hình của bạn
 
 # Gửi lệnh phát bài nhạc 001_music.mp3 trong thư mục 01
 command = bytes([0x7E, 0xFF, 0x06, 0x03, 0x01, 0x01, 0x00, 0x7E])
